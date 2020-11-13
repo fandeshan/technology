51a0629cfd1eee023fd7224d029927b7c5d1e364
pa_fandeshan <fandeshan761@pingan.com.cn>
2020-11-13 17:46:38 +0800

    提交leetcode 236. 二叉树的最近公共祖先

---
+++ Algorithm&DataStructure/leetcode/src/lowest-common-ancestor-of-a-binary-tree/main.go
@@ -0,0 +1,33 @@
+package main
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+func main() {
+	
+}
+
+/**
+ * Definition for TreeNode.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *ListNode
+ *     Right *ListNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left,p,q)
+	right := lowestCommonAncestor(root.Right,p,q)
+	if left != nil && right != nil {
+		return root
+	} else if left != nil {
+		return left
+	} else if right != nil {
+		return right
+	}
+	return nil
+}

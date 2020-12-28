31ddc0a336ae35da3d4953fea226124355d01f3b
fandeshan <804937493@qq.com>
2020-12-28 14:49:26 +0800

    提交leetcode 714. 买卖股票的最佳时机含手续费

--- Algorithm&DataStructure/leetcode/src/best-time-to-buy-and-sell-stock-with-transaction-fee/main.go
+++ Algorithm&DataStructure/leetcode/src/best-time-to-buy-and-sell-stock-with-transaction-fee/main.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println(maxProfit([]int{1, 3, 2, 8, 4, 9},2))
 }
 
 func maxProfit(prices []int, fee int) int {
+	n := len(prices)
+	if n < 2 {
+		return 0
+	}
+	a := 0
+	b := math.MinInt32
+	for i :=0 ; i < n ;i++ {
+		tmp := a
+		a = max(tmp, b+prices[i])
+		b = max(b,tmp-prices[i]-fee)
+	}
+	return a
+}
+func max(a,b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxProfit1(prices []int, fee int) int {
 	n := len(prices)
 	if n < 2 {
 		return 0

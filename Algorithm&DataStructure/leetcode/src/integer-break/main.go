be1604b38719522bf6db0ebfb239f43b96289eea
fandeshan <804937493@qq.com>
2020-12-30 11:10:42 +0800

    提交leetcode 343. 整数拆分

---
+++ Algorithm&DataStructure/leetcode/src/integer-break/main.go
@@ -0,0 +1,57 @@
+package main
+
+func main() {
+
+}
+
+func integerBreak(n int) int {
+	if n < 2 {
+		return 0
+	}
+	if n <= 3 {
+		return n-1
+	}
+	if n%3==0 {
+		return myPow(3,n/3)
+	} else if n%3==1 {
+		return myPow(3,n/3-1) * 4
+	}
+	return myPow(3,n/3)*2
+}
+
+func myPow(x int, n int) int {
+	negative := false
+	if n < 0 {
+		negative = true
+		n = -n
+	}
+
+	result := 1
+	for{
+		tmp := x
+		if n == 0 {
+			break
+		}
+		if n == 1 {
+			result = tmp * result
+			break
+		}
+
+		i := 2
+		for ; i <= n; {
+			tmp = tmp * tmp
+			if i*i > n {
+				break
+			}
+			i = i + i
+		}
+		result = tmp*result
+		n = n-i
+	}
+
+
+	if negative {
+		return 1/result
+	}
+	return result
+}

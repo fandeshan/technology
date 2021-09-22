d7f9a630ea79499b03c11c5763834472479d3a45
fandeshan <804937493@qq.com>
2021-09-22 10:26:01 +0800

    commit leetcode :725. 分隔链表

---
+++ Algorithm&DataStructure/leetcode/src/split_linked_list_in_parts/Solution.java
@@ -0,0 +1,113 @@
+package split_linked_list_in_parts;
+
+/**
+ * @author fandeshan
+ * @description 725. 分隔链表
+ * 给你一个头结点为 head 的单链表和一个整数 k ，请你设计一个算法将链表分隔为 k 个连续的部分。
+ *
+ * 每部分的长度应该尽可能的相等：任意两部分的长度差距不能超过 1 。这可能会导致有些部分为 null 。
+ *
+ * 这 k 个部分应该按照在链表中出现的顺序排列，并且排在前面的部分的长度应该大于或等于排在后面的长度。
+ *
+ * 返回一个由上述 k 部分组成的数组。
+ *
+ *  
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3], k = 5
+ * 输出：[[1],[2],[3],[],[]]
+ * 解释：
+ * 第一个元素 output[0] 为 output[0].val = 1 ，output[0].next = null 。
+ * 最后一个元素 output[4] 为 null ，但它作为 ListNode 的字符串表示是 [] 。
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,2,3,4,5,6,7,8,9,10], k = 3
+ * 输出：[[1,2,3,4],[5,6,7],[8,9,10]]
+ * 解释：
+ * 输入被分成了几个连续的部分，并且每部分的长度相差不超过 1 。前面部分的长度大于等于后面部分的长度。
+ *  
+ *
+ * 提示：
+ *
+ * 链表中节点的数目在范围 [0, 1000]
+ * 0 <= Node.val <= 1000
+ * 1 <= k <= 50
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/split-linked-list-in-parts
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/9/22
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int len = 0;
+        ListNode curr = head;
+        while (curr != null){
+            len ++;
+            curr = curr.next;
+        }
+        int splitLen = len/k;
+        int mod = len%k;
+        ListNode[] nodes = new ListNode[k];
+        curr = head;
+        for (int i = 0; i < nodes.length; i++) {
+            ListNode prev = new ListNode(-1,curr);
+            nodes[i] = curr;
+            for (int j = 0; j < splitLen; j++) {
+                curr = curr.next;
+                prev = prev.next;
+            }
+            if (i < mod){
+                curr = curr.next;
+                prev = prev.next;
+            }
+            prev.next = null;
+        }
+        return nodes;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(7);
+        ListNode node8 = new ListNode(8);
+        ListNode node9 = new ListNode(9);
+        ListNode node10 = new ListNode(10);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        node8.next = node9;
+        node9.next = node10;
+        System.out.println(new Solution().splitListToParts(node1,3));
+    }
+
+
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}

ada04e15123d815262df2448dfba70c46cd65a37
fandeshan <804937493@qq.com>
2021-12-03 18:24:26 +0800

    commit leetcode :1005. K 次取反后最大化的数组和

---
+++ Algorithm&DataStructure/leetcode/src/maximize_sum_of_array_after_k_negations/Solution.java
@@ -0,0 +1,71 @@
+package maximize_sum_of_array_after_k_negations;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * @author fandeshan
+ * @description 1005. K 次取反后最大化的数组和
+ * 给你一个整数数组 nums 和一个整数 k ，按以下方法修改该数组：
+ *
+ * 选择某个下标 i 并将 nums[i] 替换为 -nums[i] 。
+ * 重复这个过程恰好 k 次。可以多次选择同一个下标 i 。
+ *
+ * 以这种方式修改数组后，返回数组 可能的最大和 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [4,2,3], k = 1
+ * 输出：5
+ * 解释：选择下标 1 ，nums 变为 [4,-2,3] 。
+ * 示例 2：
+ *
+ * 输入：nums = [3,-1,0,2], k = 3
+ * 输出：6
+ * 解释：选择下标 (1, 2, 2) ，nums 变为 [3,1,0,2] 。
+ * 示例 3：
+ *
+ * 输入：nums = [2,-3,-1,5,-4], k = 2
+ * 输出：13
+ * 解释：选择下标 (1, 4) ，nums 变为 [2,3,-1,5,4] 。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 104
+ * -100 <= nums[i] <= 100
+ * 1 <= k <= 104
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximize-sum-of-array-after-k-negations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/12/3
+ */
+public class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        // 将数组按照绝对值大小从大到小排序，注意要按照绝对值的大小
+        nums = IntStream.of(nums)
+                .boxed()
+                .sorted((o1, o2) -> Math.abs(o2) - Math.abs(o1))
+                .mapToInt(Integer::intValue).toArray();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            //从前向后遍历，遇到负数将其变为正数，同时K--
+            if (nums[i] < 0 && k > 0) {
+                nums[i] = -nums[i];
+                k--;
+            }
+        }
+        // 如果K还大于0，那么反复转变数值最小的元素，将K用完
+
+        if (k % 2 == 1) nums[len - 1] = -nums[len - 1];
+        return Arrays.stream(nums).sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().largestSumAfterKNegations(new int[]{4,2,3},1));
+        System.out.println(new Solution().largestSumAfterKNegations(new int[]{3,-1,0,2},3));
+    }
+}

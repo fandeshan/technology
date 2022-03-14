d5c11adfd877b8262b8b36b8b3c406adc8e2b475
fandeshan <804937493@qq.com>
2022-03-14 11:43:20 +0800

    commit leetcode :599. 两个列表的最小索引总和

---
+++ Algorithm&DataStructure/leetcode/src/minimum_index_sum_of_two_lists/Solution.java
@@ -0,0 +1,72 @@
+package minimum_index_sum_of_two_lists;
+
+import java.util.*;
+
+/**
+ * @author fandeshan
+ * @description 599. 两个列表的最小索引总和
+ * 假设 Andy 和 Doris 想在晚餐时选择一家餐厅，并且他们都有一个表示最喜爱餐厅的列表，每个餐厅的名字用字符串表示。
+ *
+ * 你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 如果答案不止一个，则输出所有答案并且不考虑顺序。 你可以假设答案总是存在。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]，list2 = ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+ * 输出: ["Shogun"]
+ * 解释: 他们唯一共同喜爱的餐厅是“Shogun”。
+ * 示例 2:
+ *
+ * 输入:list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]，list2 = ["KFC", "Shogun", "Burger King"]
+ * 输出: ["Shogun"]
+ * 解释: 他们共同喜爱且具有最小索引和的餐厅是“Shogun”，它有最小的索引和1(0+1)。
+ *  
+ *
+ * 提示:
+ *
+ * 1 <= list1.length, list2.length <= 1000
+ * 1 <= list1[i].length, list2[i].length <= 30 
+ * list1[i] 和 list2[i] 由空格 ' ' 和英文字母组成。
+ * list1 的所有字符串都是 唯一 的。
+ * list2 中的所有字符串都是 唯一 的。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-index-sum-of-two-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2022/3/14
+ */
+public class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String,Integer> retMap = new HashMap<>();
+        for (int i = 0; i < list1.length; i++) {
+            retMap.put(list1[i],i);
+        }
+        List<String> result = new ArrayList<String>();
+        int minIndex = Integer.MAX_VALUE;
+        int minIndexList1 = Integer.MAX_VALUE;
+        for (int i = 0; i < list2.length; i++) {
+            if (retMap.containsKey(list2[i])){
+                int index = retMap.get(list2[i]) + i;
+                if (minIndex > index){
+                    minIndex = index;
+                    minIndexList1 = retMap.get(list2[i]);
+                }
+            }
+        }
+        for (int i = 0; i < list2.length; i++) {
+            if (retMap.containsKey(list2[i]) && (minIndex == retMap.get(list2[i]) + i)){
+                result.add(list2[i]);
+            }
+        }
+        return result.toArray(new String[result.size()]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findRestaurant(new String[]{"Shogun", "Tapioca Express", "Burger King", "KFC"},
+                new String[]{"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"}));
+
+        System.out.println(new Solution().findRestaurant(new String[]{"Shogun", "Tapioca Express", "Burger King", "KFC"},
+                new String[]{"KFC", "Shogun", "Burger King"}));
+    }
+}

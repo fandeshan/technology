acabdefc13a9da59f58367f677ae88903cbb1845
fandeshan <804937493@qq.com>
2022-03-31 16:42:07 +0800

    commit leetcode :100. 相同的树

---
+++ Algorithm&DataStructure/leetcode/src/same_tree/Solution.java
@@ -0,0 +1,87 @@
+package same_tree;
+
+/**
+ * @author fandeshan
+ * @description 100. 相同的树
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：p = [1,2,3], q = [1,2,3]
+ * 输出：true
+ * 示例 2：
+ *
+ *
+ * 输入：p = [1,2], q = [1,null,2]
+ * 输出：false
+ * 示例 3：
+ *
+ *
+ * 输入：p = [1,2,1], q = [1,1,2]
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * 两棵树上的节点数目都在范围 [0, 100] 内
+ * -104 <= Node.val <= 104
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/same-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2022/3/31
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null){
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val){
+            return false;
+        }
+        return  isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        TreeNode q = new TreeNode(1);
+
+        System.out.println(new Solution().isSameTree(p,q));
+    }
+}
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

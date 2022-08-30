d60aae57251e8c9cecedd14762a3e2636f41745b
fandeshan <804937493@qq.com>
2022-08-30 10:37:56 +0800

    commit leetcode :654. 最大二叉树

---
+++ Algorithm&DataStructure/leetcode/src/maximum_binary_tree/Solution.java
@@ -0,0 +1,106 @@
+package maximum_binary_tree;
+
+/**
+ * @author fandeshan
+ * @description 654. 最大二叉树
+ *
+ * 给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
+ *
+ * 创建一个根节点，其值为 nums 中的最大值。
+ * 递归地在最大值 左边 的 子数组前缀上 构建左子树。
+ * 递归地在最大值 右边 的 子数组后缀上 构建右子树。
+ * 返回 nums 构建的 最大二叉树 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [3,2,1,6,0,5]
+ * 输出：[6,3,5,null,2,0,null,null,1]
+ * 解释：递归调用如下所示：
+ * - [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+ *     - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+ *         - 空数组，无子节点。
+ *         - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+ *             - 空数组，无子节点。
+ *             - 只有一个元素，所以子节点是一个值为 1 的节点。
+ *     - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+ *         - 只有一个元素，所以子节点是一个值为 0 的节点。
+ *         - 空数组，无子节点。
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,2,1]
+ * 输出：[3,null,2,null,1]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] <= 1000
+ * nums 中的所有整数 互不相同
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/maximum-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @date 2022/8/30
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return dfs(nums,0,nums.length - 1);
+    }
+    private TreeNode dfs(int[] nums,int start,int end){
+        if (start > end){
+            return null;
+        }
+        if (start == end){
+            return new TreeNode(nums[start]);
+        }
+        int maxIndex = start;
+        for (int i = start + 1; i <=end ; i++) {
+            if (nums[i] > nums[maxIndex]){
+                maxIndex = i;
+            }
+        }
+        TreeNode node = new TreeNode(nums[maxIndex]);
+        node.left = dfs(nums,start,maxIndex-1);
+        node.right = dfs(nums,maxIndex+1,end);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new Solution().constructMaximumBinaryTree(new int[]{3,2,1,6,0,5});
+        System.out.println(node);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
